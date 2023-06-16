8499200960edf35e1c76799dac173714a53a4776
pavelmzs <uporov155@gmail.com>
2023-06-16 17:14:28 +0300

    start

---
+++ GameMaze/Assets/Scripts/UI/UI Scaling/Backgraund_Scaling.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Backgraund_Scaling : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.GetComponent<RectTransform>().sizeDelta = new Vector2(Screen.height, Screen.height);
+    }
+}

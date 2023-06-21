899b5ba7eef1d03dc030ab6876e3b0e0fcd3d76d
pavelmzs <uporov155@gmail.com>
2023-06-21 12:55:37 +0300

    Fixing

--- GameMaze/Assets/Scripts/Game/CameraFollow.cs
+++ GameMaze/Assets/Scripts/Game/CameraFollow.cs
@@ -7,7 +7,7 @@ public class CamerasFollow : MonoBehaviour
     public Transform target;
     public float smoothSpeed = 0.125f;
     public Vector3 offset;
-    private void LateUpdate()//Камера За героем
+    private void FixedUpdate()//Движения героя
     {
         Vector3 desiredPosition = new Vector3(target.position.x + offset.x, target.position.y + offset.y, transform.position.z);
         Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
